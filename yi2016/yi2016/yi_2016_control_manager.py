2c4bb08ff7f62a2e82f493b683af0e130434b09a
shikito <shikito.aos@gmail.com>
2020-08-27 17:21:34 +0900

    modify controll manager

--- yi2016/yi2016/yi_2016_control_manager.py
+++ yi2016/yi2016/yi_2016_control_manager.py
@@ -12,6 +12,7 @@ from yi2016_utils.multi_array import encode_array, decode_array
 class Yi2016ControlManager(Node):
     def __init__(self):
         super().__init__("yi_2016_control_manager")
+        self.isSetController = False
 
         # Target Object Surface Preference.
         self.cli_target_object = self.create_client(
@@ -21,32 +22,59 @@ class Yi2016ControlManager(Node):
         self.req = Float64MultiArray.Request()
 
         # Control State Publisher
-        self.pub_control_state = self.create_publisher(
-            ControlState, 'control_state', 10
-        )
+        self.pub_control_state = self.create_publisher(ControlState, 'control_state', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.pub_control_state_callback)
+        self.iter_num = 0
+
+        # Main Loop
+        control_period = 0.5
+        self.control_period = self.create_timer(control_period, self.control_loop)
 
         # Display Result Preference.
+
+    # Algorithm 1 Loop in Yi Paper
+    def control_loop(self):
+        self.get_logger().info('control_loop')
+        x_star = self.controller.calc_next_point()  # Loop 1) 2) 3)
+        self.get_logger().info('done calc_next_point')
+        self.get_logger().info(str(x_star.x))
+        y_star = self.obtain_y_at_x(x_star.x)            # Loop 4)
+        self.get_logger().info('done obtain_y_at_x')
+        self.controller.add_x_y_to_T(x_star.x, y_star)   # Loop 5)
+
+    def pub_control_state_callback(self):
+        msg = ControlState()
+        msg.iter_num = self.iter_num
+        self.pub_control_state.publish(msg)
+        self.get_logger().info(f'Iteration : {msg.iter_num}')
 
     # Algorithm 1 Loop 4) in Yi Paper
     def obtain_y_at_x(self, x):
         self.req = encode_array(array=x, comm_data=self.req)
-        self.future = self.cli.call_async(self.req)
-        while rclpy.ok():
-            rclpy.spin_once()
+        self.future = self.cli_target_object.call_async(self.req)
+        self.get_logger().info('waaai')
+        while True:
+            if not self.isSetController:
+                rclpy.spin_once(self)
             if self.future.done():
                 try:
+                    self.get_logger().info('try')
                     response = self.future.result()
                 except Exception as e:
                     self.get_logger().info(
                         'Service call failed %r' % (e,))
                 else:
                     self.get_logger().info('Success!')
-                break
+                    break
         y = decode_array(response)
+        self.get_logger().info('get_y')
         return y
 
     def set_controller(self, controller):
         self.controller = controller
+        self.isSetController = True
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -66,6 +94,15 @@ def main(args=None):
         initial_points=initial_points)
     control_manager.set_controller(yi_controller)
 
+    # Activate ControlManager
+    rclpy.spin(control_manager)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    control_manager.destroy_node()
+    rclpy.shutdown()
+    
     import ipdb; ipdb.set_trace()
 
 
